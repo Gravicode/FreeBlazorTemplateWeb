6ca0db1970b096f5d903cc060bd108b397d1acb7
mifmasterz <mifmasterz@yahoo.com>
2022-02-27 07:39:03 +0700

    init

---
+++ SBUIKIT/SBUIKitBlazor/Data/AppConfig.cs
@@ -0,0 +1,12 @@
+﻿namespace SBUIKitBlazor.Data
+{
+    public class AppConfig
+    {
+        //public enum ThemeStyles
+        //{
+        //    Dark,Light
+        //}
+        //public static ThemeStyles CurrentTheme = ThemeStyles.Light;
+        public static string AppName = "SB UI Kit Template";
+    }
+}
